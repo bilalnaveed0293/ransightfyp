4c9cb71a611cde5ccda00895a2ce3a617b4ad1cb
bilal naveed <114847930+bilalnaveed0293@users.noreply.github.com>
2026-03-30 19:58:19 +0500

    Update LSTM model file extension from .h5 to .keras

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def load_resources():
     # Load CNN
     cnn = tf.keras.models.load_model("cnn1.keras")
     # Load LSTM
-    lstm = tf.keras.models.load_model("ransomware_lstm_dynamic.h5")
+    lstm = tf.keras.models.load_model("ransomware_lstm_dynamic.keras")
     # Load Tokenizer
     with open("tokenizer.pkl", "rb") as f:
         tok = pickle.load(f)
